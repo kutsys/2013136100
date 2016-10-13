05beffb208ef62251aaf553827a0bcac1820a1ae
KoreaTech-System Programming 2016 <gkgkgy@koreatech.ac.kr>
2016-10-14 00:29:19 +0900

    Update high_level.c

--- high_level.c
+++ high_level.c
@@ -8,32 +8,36 @@
 
 int main(char* argv[])
 {
-    char data[1024];
+    char block[1024];
     int nread;
     FILE* in;
     FILE* out;
-    clock_t newtime = 0;
+    //동작 시간을 측정하기 위함
+    clock_t newtime = 0; 
     clock_t oldtime = 0;
     double ElapsedTime = 0.0;
+	
     char msg[16] = "Process in";
 
-    in = fopen("test.zip","rb");
-    out = fopen("copied.zip","wb");
+	//text.zip이 파일을 이너리 파일 읽기 전용으로 열기
+    in = fopen("text.zip","rb");
+	//copy.zip이라는 파일을 이너리 파일 쓰기 전용으로 열기
+    out = fopen("copy.zip","wb");
 
     write(1,msg,16);
     
     oldtime = clock();
 
-    while((nread = fread(data,sizeof(char),sizeof(data),in)) > 0){
+    while((nread = fread(block,sizeof(char),sizeof(block),in)) > 0){
 	
-	fwrite(data,sizeof(char),nread,out);
+	fwrite(block,sizeof(char),nread,out);
 	
 	newtime = clock();
 	ElapsedTime = (double)(newtime - oldtime)/CLOCKS_PER_SEC;
 
-	if(ElapsedTime >= 0.5) {
+	if(ElapsedTime >= 0.1) { //만약에 실행시간이 0.1보다 크다면
 	    oldtime = clock();
-            write(1,"*",1);
+            write(1,"*",1);//"*"을 출력
 	    fflush(stdout);
 	}
 	

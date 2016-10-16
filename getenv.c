564225a5233a5f89f171434063878dbd2c18572b
KoreaTech-System Programming 2016 <gkgkgy@koreatech.ac.kr>
2016-10-17 02:16:27 +0900

    Update getenv.c

--- getenv.c
+++ getenv.c
@@ -1,14 +1,16 @@
 
-	
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-	printf( "%s = %s\n", "HOME", getenv( "HOME"));
-	printf( "%s = %s\n", "PS1", getenv( "PS1"));
-	printf( "%s = %s\n", "PATH", getenv( "PATH"));
-	printf( "%s = %s\n", "LD_LIBRARY_PATH ", getenv( "LD_LIBRARY_PATH "));
+	printf( "%s = %s\n", "$HOME", getenv( "HOME"));
+	printf( "%s = %s\n", "$PS1", getenv( "PS1"));
+	printf( "%s = %s\n", "$PATH", getenv( "PATH"));
+	printf( "%s = %s\n", "$LD_LIBRARY_PATH ", getenv( "LD_LIBRARY_PATH "));
 
+	printf("\nDefine the setenv\n");
+	setenv("TEST_ENV", "1234",0); 
+	printf("$TEST_ENV = %s\n", getenv("TEST_ENV"));
    return 0;
 }

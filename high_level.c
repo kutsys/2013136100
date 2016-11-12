c98441092594f98a5e983c24820fae9053ce5680
KoreaTech-System Programming 2016 <gkgkgy@koreatech.ac.kr>
2016-11-12 14:22:32 +0900

    Update high_level.c

--- high_level.c
+++ high_level.c
@@ -1,49 +1,29 @@
-#include<unistd.h>
-#include<sys/stat.h>
-#include<fcntl.h>
-#include<stdlib.h>
-#include<time.h>
-#include<string.h>
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
 
-int main(char* argv[])
-{
-    char block[1024];
-    int nread;
-    FILE* in;
-    FILE* out;
-    //동작 시간을 측정하기 위함
-    clock_t newtime = 0; 
-    clock_t oldtime = 0;
-    double ElapsedTime = 0.0;
-	
-    char msg[16] = "Process in";
+#define BUF_SIZE 512
 
-	//text.zip이 파일을 이너리 파일 읽기 전용으로 열기
-    in = fopen("text.zip","rb");
-	//copy.zip이라는 파일을 이너리 파일 쓰기 전용으로 열기
-    out = fopen("copy.zip","wb");
+int main(){
 
-    write(1,msg,16);
-    
-    oldtime = clock();
-
-    while((nread = fread(block,sizeof(char),sizeof(block),in)) > 0){
-	
-	fwrite(block,sizeof(char),nread,out);
-	
-	newtime = clock();
-	ElapsedTime = (double)(newtime - oldtime)/CLOCKS_PER_SEC;
-
-	if(ElapsedTime >= 0.1) { //만약에 실행시간이 0.1보다 크다면
-	    oldtime = clock();
-            write(1,"*",1);//"*"을 출력
-	    fflush(stdout);
-	}
+	char block[BUF_SIZE];
+	FILE* in;
+	FILE* out;
+	int nread;
 	
 
-    }
-    fclose(in);
-    fclose(out);
+	in=fopen("very.avi", "r");
+	out = fopen("highfile.out", "w");
 
+	
+	while((nread=fread(block, 1, BUF_SIZE, in))>0)
+		printf(".\n");
+		fwrite(block, 1, nread, out);
+	sleep(5);
+	
+
+	fclose(in);
+	fclose(out);
+	exit(0);
 }

0f5102d7a03f86474a7e1ad7231691bfa645a91b
KoreaTech-System Programming 2016 <gkgkgy@koreatech.ac.kr>
2016-12-07 05:45:06 +0900

    Update low_level.c

--- low_level.c
+++ low_level.c
@@ -1,33 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
+//다른 헤더파일에 영향을 미치는 POSIX규격에 관련한 플래그를 정의하기에 가장 첫줄에 표기
 #include <unistd.h>
-#include <fcntl.h>
+//파일 상태에 대한 정보를 얻어오는 헤더파일
 #include <sys/stat.h>
-#include <time.h>
+//이미 열려있는 파일의 특성 제어를 위하여 사용 fncl함수를 포함한 헤더파일
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-#define BUF_SIZE 1024
+int main()  {
+  char block[1024];
+  int in, out;
+  int nread;
+  int cnt = 0;
+  
+  //O_RDONLY : 읽기 전용으로 파일을 연다. 
 
-int main(){
-	char block[BUF_SIZE];
-	int in, out;
-	int nread;
-	time_t start, end;
-	int dif;
-
-
-	in = open("very.avi", O_RDONLY);
-	out = open("lowfile.out", O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR);
-
-	printf("start\n");
-	while((nread=read(in, block, BUF_SIZE))>0)
-		printf(".\n");
-		write(out, block, nread);
-		sleep(5);
-	printf("finish\n");
-
-	
-
-	close(in);
-	close(out);
-	exit(0);
+  in = open("abc.txt", O_RDONLY);
+  
+  //O_WRONLY : 쓰기 전용으로 파일을 연다
+	//O_CREAT : 필요하다면 파일을 생성, mode에 주어진 권한을 적용
+	//S_IRUSR : 현재 사용자에 read권한 부여
+	//I_WUSRT : 현재 사용자에 write권한 부여
+	//복사될 프로그램 명 : file.out
+  out = open("low.out", O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+  
+  /*
+	read(int filedescriptor, void *buf, size_t count)
+	filedescriptor : 읽기 작업을 수행할 파일에 대한 파일 기술자
+	buf : 파일로 부터 읽어 들인 내용을 저장하기 위한 공간
+	count : 읽어 들일 파일의 내용 크기를 지정
+	*/
+  while((nread = read(in, block, sizeof(block))) > 0) {
+    /*
+		write(int filedescriptor, void *buf, size_count)
+		buf : 파일로 부터 쓰련느 내용을 저장하기 위한 공간
+		count : 버퍼에 있는 데이터 중 실제로 파일을 저장할 데이터 크기를 지정
+		*/
+    write(out, block, nread);
+    cnt++;
+    //버퍼의 크기를 1024byte 블록 단위로 파일을 복사
+		//1024byte가 되었을 때 "."을 출력
+    if(cnt%1024 == 0) printf(".");
+  }
+  exit(0);
 }
